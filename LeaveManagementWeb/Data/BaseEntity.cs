9b196ad337c82be6938837703c5b6afc207d8d85
Eric J Dube <ericjdube@yahoo.com>
2023-06-07 12:19:22 -0400

    Added database changes and migrations.  End implement database section.

---
+++ LeaveManagementWeb/Data/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace LeaveManagementWeb.Data
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public DateTime DateModified { get; set; }
+
+    }
+}
